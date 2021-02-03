901faae03764bc07c62c4b480723efabd3826cb1
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2021-02-02 21:42:59 -0800

    Work finished
    -Can read from text files
    -Displays info from text files

--- ReadingFiles3/ReadingFiles3/Game1.cs
+++ ReadingFiles3/ReadingFiles3/Game1.cs
@@ -8,17 +8,21 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System.IO;
 
 namespace ReadingFiles3
 {
     /// <summary>
     /// This is the main type for your game
     /// </summary>
+    /// 
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        SpriteFont font;
+        List<string> lines;
+        int inputX, inputY;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -34,7 +38,7 @@ namespace ReadingFiles3
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            lines = new List<string>();
             base.Initialize();
         }
 
@@ -42,14 +46,57 @@ namespace ReadingFiles3
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
+        /// 
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("SpriteFont1");
+            ReadFileAsStrings(@"Content/anything.txt");
 
+            ReadFileOfIntegers(@"Content/numbers.txt");
             // TODO: use this.Content to load your game content here
         }
-
+        private void ReadFileAsStrings(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        lines.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        private void ReadFileOfIntegers(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] parts = line.Split(' ');
+                        inputX = Convert.ToInt32(parts[0]);
+                        inputY = Convert.ToInt32(parts[1]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
         /// all content.
@@ -84,8 +131,24 @@ namespace ReadingFiles3
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            //DrawText();
+            DrawNumbers();
+            spriteBatch.End();
             base.Draw(gameTime);
+        }
+        private void DrawText() {
+            Vector2 position = new Vector2(50, 10);
+            foreach (string s in lines) {
+                spriteBatch.DrawString(font, s, position, Color.Black);
+                position.Y += 30;
+            }
+        }
+        private void DrawNumbers() {
+            Vector2 position = new Vector2(50, 10);
+            int sum = inputX + inputY;
+            string answer = "I read " + inputX + " and " + inputY + ". The sum is " + sum;
+            spriteBatch.DrawString(font,answer,position,Color.Black);
         }
     }
 }
